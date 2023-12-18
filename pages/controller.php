523d0d4f17e580831425ff383eeec126dc3c1a40
JavaMFadlan <polisilol48@gmail.com>
2023-12-18 22:16:47 +0700

    [UPDATE] login and home

---
+++ pages/controller.php
@@ -0,0 +1,8 @@
+<?php 
+
+if ($_POST['email'] == 'admin@gmail.com' && $_POST['password'] == '12345678') {
+    header('location: home.php');
+}else {
+    echo '<script>alert("Invalid email or password"); window.location.href = "login.php";</script>';
+}
+?>
