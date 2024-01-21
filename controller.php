c8630b5e894e25a41cbbf678fedb210ce496e19c
FadlanAdila <muhammadfadlan1944@gmail.com>
2024-01-21 19:47:36 +0700

    [UPDATE] tugas 2

--- controller.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-if ($_POST['email'] == 'admin@gmail.com' && $_POST['password'] == '12345678') {
-    header('location: home.php');
-}else {
-    echo '<script>alert("Invalid email or password"); window.location.href = "index.php";</script>';
-}
-?>
